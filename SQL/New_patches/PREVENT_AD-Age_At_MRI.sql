7f2d58eb47344d0be620b1a00d12b6750b67d75d
John Saigle <john.saigle@gmail.com>
2019-04-12 16:13:28 -0400

    Add age at MRI script and SQL patch

---
+++ SQL/New_patches/PREVENT_AD-Age_At_MRI.sql
@@ -0,0 +1,1 @@
+ALTER TABLE candidate ADD COLUMN Age_At_MRI int(3) unsigned DEFAULT NULL;
--- tools/age_at_mri.php
+++ tools/age_at_mri.php
@@ -1,13 +1,34 @@
-<?php
+<?php declare(strict_types=1);
 
 require_once 'generic_includes.php';
 
 
-$query = "SELECT c.DoB,mpf.Date_taken
+$query = "SELECT c.CandID,c.DoB,mpf.Date_taken
     FROM candidate c
     INNER JOIN session s on s.CandID = c.CandID
     INNER JOIN flag f on s.ID = f.SessionID
     INNER JOIN mri_parameter_form mpf on mpf.CommentID = f.CommentID
     WHERE f.test_name='mri_parameter_form'";
 $result = \Database::singleton()->pselect($query, array());
-print_r($result);
+
+$template = "UPDATE candidate SET Age_At_MRI = '%s' WHERE CandID ='%s';";
+
+$UPDATEQueue = array();
+$skipped = 0;
+
+foreach ($result as $row) {
+    if (empty($row['Date_taken'] || empty($row['CandID']))) {
+        $skipped++;
+        continue;
+    }
+    $age = \Utility::calculateAge($row['DoB'], $row['Date_taken'])['year'];
+    $UPDATEQueue[] = sprintf($template, $age, $row['CandID']);
+}
+
+echo implode("\n", $UPDATEQueue);
+
+$skippedMessage = "\nSkipped $skipped candidates due to missing DoB in candidate table " .
+    "or missing Date_taken in mri_parameter_form table.\n";
+
+fwrite(STDERR, $skippedMessage);
+
